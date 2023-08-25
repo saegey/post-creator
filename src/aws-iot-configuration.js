be98d93209e441e5c11f23ffb2582024c862124b
Adam Saegebarth <adam.saegebarth@gmail.com>
2023-08-25 15:20:45 -0700

    update schema

--- src/aws-iot-configuration.js
+++
@@ -1,7 +0,0 @@
-var awsIotConfiguration = {
-  endpoint: 'wss://a29ieb9zd32ips-ats.iot.us-east-1.amazonaws.com/mqtt',
-  region: 'us-east-1',
-  poolId: 'us-east-1:bcf0ca9e-c32b-4424-b29a-49a787af50a1',
-  host: 'a29ieb9zd32ips-ats.iot.us-east-1',
-};
-module.exports = awsIotConfiguration;
