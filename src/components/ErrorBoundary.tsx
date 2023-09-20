68b20cc426ae12f1f335ff30abb2658ee59d3cf6
Adam Saegebarth <saegey@users.noreply.github.com>
2023-09-20 13:26:48 -0700

    debug 500 (#40)

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+type MyState = {
+  hasError: boolean
+}
+
+type MyProps = {
+  fallback: React.JSX.Element
+  children: React.JSX.Element
+}
+
+class ErrorBoundary extends React.Component<MyProps, MyState> {
+  state: MyState = {
+    hasError: false
+  }
+  constructor(props: MyProps) {
+    super(props);
+    // this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
