942457610349d017285e69d68719f0d14e1b9e0d
Adam Saegebarth <saegey@users.noreply.github.com>
2023-10-04 15:31:06 -0700

    seo (#51)
    * seo
    
    * fix

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,14 @@
 import { withSSRContext } from 'aws-amplify';
 import React from 'react';
 import Head from 'next/head';
+import { getCldOgImageUrl } from 'next-cloudinary';
 
 import { getPostInitial } from '../../src/graphql/customQueries';
 import { getActivity } from '../../src/actions/PostGet';
 import AuthCustom from '../../src/components/AuthCustom';
 import PostView from '../../src/components/PostView';
 import SlatePublish from '../../src/components/SlatePublish';
+
 
 type ServerSideProps = {
   req: object;
@@ -56,11 +58,57 @@ export const getServerSideProps = async ({ req, params }: ServerSideProps) => {
 const Publish = ({ post, activity }): JSX.Element => {
   const config = SlatePublish({ post, activity });
   const components = JSON.parse(post.components);
+
+  const url = getCldOgImageUrl({
+    src: JSON.parse(post.heroImage).public_id,
+  });
+
   return (
     <AuthCustom>
       <Head>
-        <title>View Post</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>View Post - {post.title}</title>
+        <meta
+          property='twitter:title'
+          name='twitter:title'
+          content={post.title}
+        />
+        <meta
+          property='twitter:description'
+          name='twitter:description'
+          content={post.subhead}
+        />
+        <meta
+          property='twitter:url'
+          name='twitter:url'
+          content={`http://mopd.us/${post.shortUrl}`}
+        />
+        <meta
+          property='twitter:card'
+          name='twitter:card'
+          content='summary_large_image'
+        />
+        <meta
+          property='og:url'
+          name='og:url'
+          content={`http://mopd.us/${post.shortUrl}`}
+        />
+        <meta
+          property='og:description'
+          name='og:description'
+          content={post.subhead}
+        />
+        <meta
+          name='author'
+          content={post.author ? post.author.fullName : 'unknown'}
+        />
+        <meta property='og:type' name='og:type' content='article' />
+
+        <link
+          rel='icon'
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>M</text></svg>"
+        />
+        <meta property='image' name='image' content={`${url}`} />
+        <meta property='og:image' name='og:image' content={`${url}`} />
       </Head>
       <PostView components={components} config={config} post={post} />
     </AuthCustom>
