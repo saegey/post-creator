fae1d5dbf6a31e30f7b582b9a56c38f8cc14a6db
Adam Saegebarth <saegey@users.noreply.github.com>
2023-10-19 11:09:30 -0700

    Fix react warnings (#73)
    * Fix react warnings
    
    * fixes

--- src/components/PostAuthor.tsx
+++ src/components/PostAuthor.tsx
@@ -9,100 +9,100 @@ import { cloudUrl } from '../utils/cloudinary';
 import { useViewport } from './ViewportProvider';
 
 interface PostAuthorProps {
-  publishedDate: string;
   postAuthor?: {
     fullName: string;
     username: string;
     image: string;
   };
 }
-const PostAuthor = ({
-  publishedDate,
-  postAuthor = undefined,
-}: PostAuthorProps) => {
+
+const PostAuthor = ({ postAuthor = undefined }: PostAuthorProps) => {
   const { width } = useViewport();
   const post = React.useContext(PostContext) as any;
   const author = postAuthor ? postAuthor : post.author;
 
   return (
-    <Box
-      sx={{
-        position: width < 1015 ? 'relative' : 'absolute',
-        marginTop: ['20px', '0px', '0px'],
-        maxWidth: '690px',
-
-        '@media only screen and (max-width: 1315px) and (min-width: 1015px)': {
-          marginX: '60px',
-        },
-        marginX: width < 1015 ? 'auto' : '200px',
-        width: width < 1015 ? '690px' : '150px',
-        height: '100%',
-        marginBottom: ['20px', '30px', '60px'],
-      }}
-      contentEditable='false'
-    >
-      <Flex
+    <div contentEditable={false}>
+      <Box
         sx={{
-          flexDirection: ['row', 'row', 'column'],
-          alignItems: 'flex-start',
-          gap: '20px',
-          marginLeft: ['10px', 0, 0],
+          position: width < 1015 ? 'relative' : 'absolute',
+          marginTop: ['20px', '0px', '0px'],
+          maxWidth: '690px',
+
+          '@media only screen and (max-width: 1315px) and (min-width: 1015px)':
+            {
+              marginX: '60px',
+            },
+          marginX: width < 1015 ? 'auto' : '200px',
+          width: width < 1015 ? '690px' : '150px',
+          height: '100%',
+          marginBottom: ['20px', '30px', '60px'],
         }}
       >
-        {author && author.image && (
-          <Box
-            sx={{
-              width: ['60px', '80px', '80px'],
-              height: ['60px', '80px', '80px'],
-            }}
-          >
-            <CldImage
-              width='400'
-              height='300'
-              src={author.image}
-              style={{
-                objectFit: 'cover',
-                width: '100%',
-                height: '100%',
-                marginTop: 'auto',
-                marginBottom: 'auto',
-                borderRadius: '100%',
+        <Flex
+          sx={{
+            flexDirection: ['row', 'row', 'column'],
+            alignItems: 'flex-start',
+            gap: '20px',
+            marginLeft: ['10px', 0, 0],
+          }}
+        >
+          {author && author.image && (
+            <Box
+              sx={{
+                width: ['60px', '80px', '80px'],
+                height: ['60px', '80px', '80px'],
               }}
-              config={{
-                cloud: {
-                  cloudName: cloudUrl,
-                },
+            >
+              <CldImage
+                priority={true}
+                width='400'
+                height='300'
+                src={author.image}
+                style={{
+                  objectFit: 'cover',
+                  width: '100%',
+                  height: '100%',
+                  marginTop: 'auto',
+                  marginBottom: 'auto',
+                  borderRadius: '100%',
+                }}
+                config={{
+                  cloud: {
+                    cloudName: cloudUrl,
+                  },
+                }}
+                // preserveTransformations
+                // underlay={user.attributes.picture}
+                quality={90}
+                sizes='100vw'
+                alt='Description of my image'
+              />
+            </Box>
+          )}
+          {author && !author.image && <AvatarButton />}
+          <Flex sx={{ flexDirection: 'column' }}>
+            <Text
+              as='span'
+              sx={{
+                fontSize: '13px',
+                fontWeight: '700',
+                lineHeight: '18px',
+                textTransform: 'uppercase',
               }}
-              // preserveTransformations
-              // underlay={user.attributes.picture}
-              quality={90}
-              sizes='100vw'
-              alt='Description of my image'
-            />
-          </Box>
-        )}
-        {author && !author.image && <AvatarButton />}
-        <Flex sx={{ flexDirection: 'column' }}>
-          <Text
-            as='span'
-            sx={{
-              fontSize: '13px',
-              fontWeight: '700',
-              lineHeight: '18px',
-              textTransform: 'uppercase',
-            }}
-          >
-            by {author?.fullName}
-          </Text>
-          <Text
-            as='span'
-            sx={{ fontSize: '13px', fontWeight: '700', lineHeight: '18px' }}
-          >
-            {moment(post.createdAt).format('MM.DD.YY')}
-          </Text>
+            >
+              by {author?.fullName}
+            </Text>
+            <Text
+              as='span'
+              sx={{ fontSize: '13px', fontWeight: '700', lineHeight: '18px' }}
+            >
+              {moment(post.createdAt).format('MM.DD.YY')}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Box>
+    </div>
   );
 };
 
