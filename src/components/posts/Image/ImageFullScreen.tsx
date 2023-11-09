2a2619144dc7dad9671c95054bbecf3556908cfc
Adam Saegebarth <adam.saegebarth@gmail.com>
2023-11-09 11:40:16 -0800

    images maximized and gallery scroll

---
+++ src/components/posts/Image/ImageFullScreen.tsx
@@ -0,0 +1,247 @@
+import { Box, Close, Flex } from "theme-ui";
+import React from "react";
+import { CldImage } from "next-cloudinary";
+
+import { cloudUrl } from "../../../utils/cloudinary";
+import { PostContext } from "../../PostContext";
+import useSwipe from "../../useSwipe";
+
+const ImageFullScreen = ({
+  setIsMaximized,
+  width,
+  height,
+  publicId,
+  index,
+}: {
+  setIsMaximized: React.Dispatch<React.SetStateAction<boolean>>;
+  width: number;
+  height: number;
+  publicId: string;
+  index: number;
+}) => {
+  const [currentImage, setCurrentImage] = React.useState<React.ReactNode>(
+    <CldImage
+      width={width}
+      height={height}
+      src={publicId}
+      sizes="100vw"
+      alt="race pic"
+      quality={90}
+      style={{
+        objectFit: "contain",
+        width: "100%",
+        maxHeight: "100%",
+        borderRadius: width && height && width > height ? "5px" : "0px",
+        // boxShadow: `${selected && focused ? "0 0 0 3px #B4D5FF" : "none"}`,
+      }}
+      config={{
+        cloud: {
+          cloudName: cloudUrl,
+        },
+      }}
+    />
+  );
+  const [currentIndex, setCurrentIndex] = React.useState(index);
+  // console.log(currentIndex);
+
+  const { images } = React.useContext(PostContext);
+  if (!images) {
+    throw Error("musts have images");
+  }
+  const swipeHandlers = useSwipe({
+    onSwipedLeft: () => {
+      nextImage();
+    },
+    onSwipedRight: () => previousImage(),
+  });
+
+  const nextImage = () => {
+    if (
+      !images ||
+      currentIndex === undefined ||
+      currentIndex === images.length - 1
+    ) {
+      return;
+    }
+    const newIndex = images.length > currentIndex + 1 ? currentIndex + 1 : 0;
+    const { width, height, public_id } = images[newIndex];
+    setCurrentImage(
+      <CldImage
+        width={width}
+        height={height}
+        src={public_id}
+        sizes="100vw"
+        alt="race pic"
+        quality={90}
+        style={{
+          objectFit: "contain",
+          width: "100%",
+          maxHeight: "100%",
+          borderRadius: width && height && width > height ? "5px" : "0px",
+        }}
+        config={{
+          cloud: {
+            cloudName: cloudUrl,
+          },
+        }}
+      />
+    );
+    setCurrentIndex(newIndex);
+  };
+
+  const previousImage = () => {
+    if (!images || currentIndex === undefined || currentIndex === 0) {
+      return;
+    }
+    const newIndex = currentIndex - 1;
+    const { width, height, public_id } = images[newIndex];
+    setCurrentImage(
+      <CldImage
+        width={width}
+        height={height}
+        src={public_id}
+        sizes="100vw"
+        alt="race pic"
+        quality={90}
+        style={{
+          objectFit: "contain",
+          width: "100%",
+          maxHeight: "100%",
+          borderRadius: width && height && width > height ? "5px" : "0px",
+        }}
+        config={{
+          cloud: {
+            cloudName: cloudUrl,
+          },
+        }}
+      />
+    );
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <Box
+      {...swipeHandlers}
+      sx={{
+        position: "fixed",
+        top: "0",
+        height: "100%",
+        width: "100%",
+        left: "0",
+        backgroundColor: "text",
+        zIndex: 200,
+        display: "flex",
+      }}
+    >
+      <Flex
+        sx={{ position: "relative", width: "100%", justifyContent: "center" }}
+      >
+        {currentImage}
+      </Flex>
+      <Flex
+        sx={{
+          justifyContent: "right",
+          padding: "15px",
+          position: "absolute",
+          width: "100%",
+          zIndex: 1,
+        }}
+      >
+        <Close
+          onClick={() => {
+            setIsMaximized(false);
+          }}
+          sx={{ color: "background" }}
+        />
+      </Flex>
+      {currentIndex < images.length - 1 && (
+        <Flex
+          sx={{
+            justifyContent: "right",
+            alignItems: "center",
+            // padding: "15px",
+            position: "absolute",
+            // width: "100%",
+            right: "0px",
+            height: "100%",
+            zIndex: 0,
+          }}
+        >
+          <Box
+            sx={{
+              padding: "10px",
+              backgroundColor: "text",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              nextImage();
+            }}
+          >
+            <Box sx={{ width: "40px", height: "auto" }}>
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 6L15 12L9 18"
+                  stroke="var(--theme-ui-colors-background)"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </Box>
+          </Box>
+        </Flex>
+      )}
+      {currentIndex > 0 && (
+        <Flex
+          sx={{
+            justifyContent: "left",
+            alignItems: "center",
+            // padding: "15px",
+            position: "absolute",
+            // width: "100%",
+            left: "0px",
+            height: "100%",
+            zIndex: 0,
+          }}
+        >
+          <Box
+            sx={{
+              padding: "10px",
+              backgroundColor: "text",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              previousImage();
+            }}
+          >
+            <Box sx={{ width: "40px", height: "auto" }}>
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 6L9 12L15 18"
+                  stroke="var(--theme-ui-colors-background)"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </Box>
+          </Box>
+        </Flex>
+      )}
+    </Box>
+  );
+};
+
+export default ImageFullScreen;
--- src/components/posts/Image/ImageViewWrapper.tsx
+++ src/components/posts/Image/ImageViewWrapper.tsx
@@ -5,65 +5,104 @@ import React from "react";
 import { ImageElementType } from "../../../types/common";
 import { cloudUrl } from "../../../utils/cloudinary";
 import { PostContext } from "../../PostContext";
+import MaximizeIcon from "../../icons/MaximizeIcon";
+import ImageFullScreen from "./ImageFullScreen";
 
 const ImageViewWrapper = ({ node }: { node: ImageElementType }) => {
   const { images } = React.useContext(PostContext);
-  const imageMeta = images?.find((i) => i.public_id === node.public_id);
+  const [isMaximized, setIsMaximized] = React.useState(false);
+  const imageMetaIndex: number | undefined = images?.findIndex(
+    (i) => i.public_id === node.public_id
+  );
+  if (imageMetaIndex === undefined) {
+    return;
+  }
+  const imageMeta = images ? images[imageMetaIndex] : undefined;
+  const wrapperRef = React.useRef();
 
   return (
-    <Flex>
-      <Box
-        sx={{
-          position: "relative",
-          width: "900px",
-          maxWidth: "900px",
-          marginX: "auto",
-          marginY: ["20px", "60px", "60px"],
-          height: "600px",
-          // maxHeight: '600px',
-          marginBottom: "20px",
-        }}
-      >
-        <Flex
+    <>
+      {isMaximized && imageMeta?.height && imageMeta?.width && (
+        <ImageFullScreen
+          setIsMaximized={setIsMaximized}
+          width={imageMeta?.width}
+          height={imageMeta?.height}
+          publicId={imageMeta.public_id}
+          index={imageMetaIndex}
+        />
+      )}
+
+      <Flex>
+        <Box
           sx={{
-            width: "100%",
+            position: "relative",
+            width: "900px",
+            maxWidth: "900px",
+            marginX: "auto",
+            marginY: ["20px", "60px", "60px"],
             height: "600px",
-            backgroundColor: imageMeta?.colors[0],
-            borderRadius: [0, "5px", "5px"],
+            // maxHeight: '600px',
+            marginBottom: "20px",
           }}
         >
-          <CldImage
-            width="1200"
-            height="1200"
-            src={node.public_id}
-            sizes="100vw"
-            alt="race pic"
-            quality={90}
-            style={{
-              objectFit: "contain",
-              // height: '100%',
+          <Flex
+            sx={{
               width: "100%",
-              maxHeight: "100%",
-              borderRadius:
-                imageMeta &&
-                imageMeta.width &&
-                imageMeta.height &&
-                imageMeta?.width > imageMeta?.height
-                  ? "5px"
-                  : "0px",
+              height: "600px",
+              backgroundColor: imageMeta?.colors[0],
+              borderRadius: [0, "5px", "5px"],
             }}
-            config={{
-              cloud: {
-                cloudName: cloudUrl,
-              },
+          >
+            <CldImage
+              width="1200"
+              height="1200"
+              src={node.public_id}
+              sizes="100vw"
+              alt="race pic"
+              quality={90}
+              style={{
+                objectFit: "contain",
+                // height: '100%',
+                width: "100%",
+                maxHeight: "100%",
+                borderRadius:
+                  imageMeta &&
+                  imageMeta.width &&
+                  imageMeta.height &&
+                  imageMeta?.width > imageMeta?.height
+                    ? "5px"
+                    : "0px",
+              }}
+              config={{
+                cloud: {
+                  cloudName: cloudUrl,
+                },
+              }}
+            />
+          </Flex>
+          <Text as="p" sx={{ paddingX: ["10px", null, null] }}>
+            {node.caption}
+          </Text>
+          <Box
+            sx={{
+              position: "absolute",
+              right: "10px",
+              bottom: "10px",
             }}
-          />
-        </Flex>
-        <Text as="p" sx={{ paddingX: ["10px", null, null] }}>
-          {node.caption}
-        </Text>
-      </Box>
-    </Flex>
+            ref={wrapperRef}
+          >
+            <Box
+              sx={{ width: "30px", height: "auto", cursor: "pointer" }}
+              onClick={() => {
+                setIsMaximized(true);
+              }}
+            >
+              <MaximizeIcon />
+            </Box>
+          </Box>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
