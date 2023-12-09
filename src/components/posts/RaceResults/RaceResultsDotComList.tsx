dd50217d9ff27698f04b6d3f5314697f0a495650
Adam Saegebarth <adam.saegebarth@gmail.com>
2023-12-09 15:03:18 -0800

    fix raceresult.com integration

--- src/components/posts/RaceResults/RaceResultsDotComList.tsx
+++ src/components/posts/RaceResults/RaceResultsDotComList.tsx
@@ -61,7 +61,7 @@ const RaceResultsDotComList = ({
                     : ""}
                 </Text>
                 <Text as="span" sx={{ marginLeft: "auto" }}>
-                  {row.FinishTime}
+                  {row.Time}
                 </Text>
               </Flex>
             );
