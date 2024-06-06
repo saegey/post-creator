a493aabeccabba381e96f655e93c5690227ad648
Adam Saegebarth <adam.saegebarth@gmail.com>
2024-06-06 12:00:20 -0700

    fix buiild

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -7,8 +7,7 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "nextjsblog": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
     }
   },
   "auth": {
