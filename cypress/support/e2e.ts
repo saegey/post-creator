7d08a91926ff02e74d6c692e3226d6feffcead6d
Adam Saegebarth <adam.saegebarth@gmail.com>
2024-09-09 12:06:15 -0700

    increase timeout

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -27,7 +27,7 @@ beforeEach(() => {
       cy.get("#email").type("adam.saegebarth@gmail.com");
       cy.get("#password").type(`yy*Em*jTLfw2v@6o_`);
       cy.get("#submit-login").click();
-      cy.wait(3000);
+      cy.wait(5000);
       cy.location("pathname").should("eq", "/");
     },
     {
