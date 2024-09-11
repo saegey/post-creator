bb051f1078096254063c13029abb5520ea05a545
Adam Saegebarth <adam.saegebarth@gmail.com>
2024-09-11 13:57:25 -0700

    fix 500 error

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,3 +1,7 @@
-const cloudUrl = process.env['NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME'];
+// const cloudUrl = process.env["NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME"];
+
+const cloudUrl =
+  process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ||
+  process.env.CLOUDINARY_CLOUD_NAME;
 
 export { cloudUrl };
