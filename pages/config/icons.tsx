624b7124a20298d56138ad399a998fc4aab54b00
Adam Saegebarth <adam.saegebarth@gmail.com>
2024-09-11 14:17:50 -0700

    add icon to page

--- pages/config/icons.tsx
+++ pages/config/icons.tsx
@@ -46,6 +46,7 @@ import RefreshIcon from "../../src/components/icons/RefreshIcon";
 import CloudCheck from "../../src/components/icons/CloudCheck";
 import ZoomOut from "../../src/components/icons/ZoomOut";
 import SaveIcon from "../../src/components/icons/SaveIcon";
+import DeleteIcon from "../../src/components/icons/DeleteIcon";
 
 const icons = [
   BulletListIcon,
@@ -83,6 +84,7 @@ const icons = [
   CloudCheck,
   ZoomOut,
   SaveIcon,
+  DeleteIcon,
 ];
 
 const ThemePreview = () => {
